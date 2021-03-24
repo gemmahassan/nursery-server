2701dbcc7b5acfc69b22ccf304c4aa80d31c3cc0
Gemma Hassan <gemmaehassan@gmail.com>
2021-03-24 21:45:33 +0000

    Adds superadmin user validation to login page redirects to admin dashboard upon successful login

--- routes/nursery.js
+++ routes/nursery.js
@@ -1,17 +1,17 @@
 const multer = require("multer");
 const upload = multer({dest: 'uploads/'});
 const nursery = require('../controllers/nursery');
+const authJwt = require("../middleware/authJwt");
 
 module.exports = app => {
   app.post('/contact', nursery.signup);
   app.put('/signup/:nurseryId', upload.single('image'), nursery.updateOnRegistration);
-  app.put('/admin/:nurseryId/approve', nursery.approve);
-  app.delete('/admin/:nurseryId/decline', nursery.decline);
+  app.put('/admin/:nurseryId/approve', authJwt.isSuperAdmin, nursery.approve);
+  app.delete('/admin/:nurseryId/decline', authJwt.isSuperAdmin, nursery.decline);
   app.get("/nurseries", nursery.getAll);
   app.get("/nurseries/confirmed", nursery.getAllConfirmedNurseries);
   app.get("/nurseries/pending", nursery.getAllPendingNurseries);
   app.get("/nurseries/:nurseryId", nursery.getNurseryById);
   app.get("/nurseries/:nurseryId/children", nursery.getAllChildren);
   app.get("/nurseries/:nurseryId/journal/add", nursery.getAllChildren);
-
 };
