e9ac460e5431f740c0af03db413f667cfe875677
Gemma Hassan <gemmaehassan@gmail.com>
2021-03-24 22:29:47 +0000

    adding config files to .gitignore

--- config/auth.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  secret: "nursery-secret-key",
-  refreshTokenSecret: "nursery-refresh-secret-key",
-  refreshTokens: [],
-};
--- config/config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  SENDGRID_API: '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'
-}
-
