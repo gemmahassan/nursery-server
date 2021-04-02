8fdd9d763b14723ece1eaa520724b5cc3a22a92c
Gemma Hassan <gemmaehassan@gmail.com>
2021-04-02 16:05:09 +0100

    [MP-64] Defaults to selected child when adding a journal entry

--- routes/journal.js
+++ routes/journal.js
@@ -1,9 +1,10 @@
 const authJwt = require("../middleware/authJwt");
+const journal = require("../controllers/journal");
+const multer = require("multer");
+const upload = multer({dest: 'uploads/'});
 
 module.exports = app => {
-  const journal = require("../controllers/journal");
-
-  app.post("/nurseries/:nurseryId/journal/add", authJwt.isAdmin, journal.addEntry);
+  app.post("/nurseries/:nurseryId/journal/add", [authJwt.isStaff, upload.single('image')], journal.addEntry);
   app.put("/child/:childId/journal/:journalId", authJwt.isAdmin, journal.update);
   app.delete("/child/:childId/journal/:journalId", authJwt.isAdmin, journal.delete);
 };
